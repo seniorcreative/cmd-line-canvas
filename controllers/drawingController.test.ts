959defdfabeeb9a75f36c88ea73b92cd2c53cab4
seniorcreative <stesmi@gmail.com>
2022-05-31 20:15:20 +1000

    Repair test for background color

--- controllers/drawingController.test.ts
+++ controllers/drawingController.test.ts
@@ -1,10 +1,17 @@
 import { canvasMethod } from './drawingController';
 import { CanvasProps } from "../types/drawingTypes";
+import DrawingModel from "../model/drawingModel";
 
 describe('matrix operations', () => {
     it('outputs the correct sized empty canvas matrix when w=4 and height=4', () => {
-        const canvasProps: CanvasProps = { width: 4, height: 4 };
-        expect(canvasMethod(canvasProps)).toEqual([['X', 'X', 'X', 'X'], ['X', ' ', ' ', 'X'], ['X', ' ', ' ', 'X'], ['X', 'X', 'X', 'X']]);
+        const backgroundColor = DrawingModel.backgroundColor;
+        const canvasProps: CanvasProps = { width: 4, height: 4, backgroundColor };
+
+        expect(canvasMethod(canvasProps)).toEqual([
+            ['X', 'X', 'X', 'X'],
+            ['X', backgroundColor, backgroundColor, 'X'],
+            ['X', backgroundColor, backgroundColor, 'X'],
+            ['X', 'X', 'X', 'X']]);
     });
 })
 
