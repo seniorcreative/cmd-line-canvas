825d22d812672f5e3de11fe78005dbc65e7cfa9e
seniorcreative <stesmi@gmail.com>
2022-06-06 09:26:20 +1000

    eslint more set up

--- src/controllers/createCanvasCommand.ts
+++ src/controllers/createCanvasCommand.ts
@@ -1,6 +1,6 @@
 import { CommandLineCanvas } from "../core/commandLineCanvas";
 import { CanvasProvider } from "../core/canvasProvider";
-import { Command } from "../model/commandTypes";
+import { Command } from '../model/commandTypes'
 
 export class CreateCanvasCommand implements Command {
 
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { createInterface } from "readline";
-import { App } from "./app";
-import { CommandParser } from "./core/commandParser";
-import { CommandFactory } from "./core/commandFactory";
-import { CanvasProvider } from "./core/canvasProvider";
+import { createInterface } from 'readline'
+import { App } from './app'
+import { CommandParser } from './core/commandParser'
+import { CommandFactory } from './core/commandFactory'
+import { CanvasProvider } from './core/canvasProvider'
 
 const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+  input: process.stdin,
+  output: process.stdout
+})
 
-const app = new App(new CommandParser(), new CommandFactory(), CanvasProvider.getInstance());
+const app = new App(new CommandParser(), new CommandFactory(), CanvasProvider.getInstance())
 
 const prompt = function () {
-    readline.question(`enter command: `, (command: string) => {
-        app.handleInput(command);
-        prompt();
-    })
-};
+  readline.question('enter command: ', (command: string) => {
+    app.handleInput(command)
+    prompt()
+  })
+}
 
-prompt();
+prompt()
