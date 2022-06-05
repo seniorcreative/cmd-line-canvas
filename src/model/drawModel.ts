fef2bec24a307721c2089af423e52ca072520ccc
seniorcreative <stesmi@gmail.com>
2022-06-05 23:02:55 +1000

    Clear out old code

--- src/model/drawModel.ts
+++ src/model/drawModel.ts
@@ -1,25 +1,8 @@
 export default class DrawModel {
 
-    private static _canvasAvailable: boolean = false;
-    private static _drawingMatrix: Array<Array<string>> = [];
     private static _instructions: any = { C: 2, L: 4, R: 4, B: 3, Q: 0 };
     private static _stashedCommands: string[] = [];
 
-    static set canvasAvailable(available: boolean) {
-        DrawModel._canvasAvailable = available;
-    }
-
-    static get canvasAvailable(): boolean {
-        return DrawModel._canvasAvailable;
-    }
-
-    static set drawingMatrix(matrix: any) {
-        DrawModel._drawingMatrix = matrix;
-    }
-
-    static get drawingMatrix(): string[][] {
-        return DrawModel._drawingMatrix;
-    }
 
     static set instructions(instructions: any) {
         DrawModel._instructions = instructions;
