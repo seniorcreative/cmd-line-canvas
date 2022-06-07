7777aaaec0b8b4fa959aaf5dd0bb081743213300
seniorcreative <stesmi@gmail.com>
2022-06-07 14:24:00 +1000

    Add operationalError import

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { CommandFactory } from "./core/commandFactory";
 import { CommandStore } from "./model/commandStore";
 import { CanvasProvider } from "./core/canvasProvider";
 import { CommandLineCanvas } from "./core/commandLineCanvas";
+import { OperationalError } from "./core/operationalError";
 
 export class App {
 
@@ -14,14 +15,23 @@ export class App {
 	) { }
 
 	public handleInput(input: string): void {
-		const commandDescriptor = this.commandParser.parse(input);
-		this.commandStore.store(commandDescriptor);
-		const command = this.commandFactory.create(commandDescriptor, this.commandStore);
-		command.execute();
+		try {
+			const commandDescriptor = this.commandParser.parse(input);
+			this.commandStore.store(commandDescriptor);
+			const command = this.commandFactory.create(commandDescriptor, this.commandStore);
+			command.execute();
 
-		const canvas: CommandLineCanvas = this.canvasProvider.canvas; // Throws an error if Canvas has not been registered by the CreateCanvasCommand before.
-		canvas.generate(this.commandStore); // Prints the canvas in the terminal.
-		canvas.render(); // Prints the canvas in the terminal.
+			const canvas: CommandLineCanvas = this.canvasProvider.canvas; // Throws an error if Canvas has not been registered by the CreateCanvasCommand before.
+			canvas.generate(this.commandStore); // Prints the canvas in the terminal.
+			canvas.render(); // Prints the canvas in the terminal.
+		} catch (e) {
+			if (e instanceof OperationalError) {
+				return console.warn(`Sorry, cannot process a command. ${e.description}.\nPlease, verify the input and try again.`);
+			} else {
+				console.error("Unexpected error happened. Exiting...");
+				process.exit(1);
+			}
+		}
 	}
 
 }
